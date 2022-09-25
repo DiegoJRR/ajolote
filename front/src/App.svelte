e71686f6934cf44ed38a80f7ffe4a567337d5550
Efrainq07 <efrainq07@hotmail.com>
2022-09-24 20:31:07 -0500

    [frontend] flowbite elements and router

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,9 +1,33 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
+	import { Router, Route, Link } from "svelte-navigator";
+  import History from "./History.svelte";
+    import Login from "./History.svelte";
 </script>
 
-<div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
-	</Alert>
-</div>
+<Router>
+    <header>
+      <h1>Ajolote</h1>
+  
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="history">History</Link>
+      </nav>
+    </header>
+  
+    <main>
+      <Route path="history">
+        <History />
+      </Route>
+  
+      <Route path="/">
+        <h3>Home</h3>
+        <p>Home sweet home...</p>
+      </Route>
+  
+      <Route path="about">
+        <h3 class="text-tahiti">About</h3>
+        <p class="text-tahiti">That's what it's all about!</p>
+      </Route>
+    </main>
+  </Router>
