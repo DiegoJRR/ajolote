12f27caa930a4911bfd9675816db325c26f60dbd
DiegoJRR <diegojrr00@gmail.com>
2022-09-25 01:29:43 -0500

    rename hook

---
+++ dashboard/src/hooks/alerts.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { createClient, SupabaseRealtimePayload } from "@supabase/supabase-js";
+import { definitions } from "../types/supabase";
+
+const supabase = createClient(
+  "https://oqymqfvmhnwgmuofdfnw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xeW1xZnZtaG53Z211b2ZkZm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQwNzI5MjQsImV4cCI6MTk3OTY0ODkyNH0.ycRqYCaM9D49uWy-bli_R3Y9KwOMNaZ5Wxh7kdqxUBc"
+);
+
+export function useCurrentAlerts(): definitions["alert"][] {
+  const [alerts, setAlerts] = useState<definitions["alert"][]>([]);
+
+  useEffect(() => {
+    supabase
+      .from("alert")
+      .select("*")
+      .then((response) => {
+        if (!response.error) {
+          console.log(response.data);
+          setAlerts(response.data);
+        } else {
+          console.log(response.error);
+        }
+      });
+  });
+
+  return alerts;
+}
