800c1f15bdafcc5e33387ae19835387050750ce0
Alam Sanchez <asancheztorres1905@gmail.com>
2022-09-25 03:33:15 -0500

    Add Time Series Chart

--- dashboard/src/pages/SignalsView.tsx
+++ dashboard/src/pages/SignalsView.tsx
@@ -1,7 +1,22 @@
+import { HStack } from '@chakra-ui/react';
+import { useMemo } from 'react';
 import SignalsTable from '../components/SignalsTable';
+import TimeSeriesChart from '../components/TimeSeriesChart';
 
 const SignalsView = () => {
-    return <SignalsTable></SignalsTable>;
+
+    
+    // const fakeData = useMemo}(() => {
+    //     const startDate = new Date().getTime() - 1000 * 60 * 60 * 24 * 30 
+    //     for (let i = 0; i < )
+    // }, []);
+
+    return (
+        <HStack>
+            {/* <TimeSeriesChart></TimeSeriesChart> */}
+            <SignalsTable></SignalsTable>
+        </HStack>
+    );
 };
 
 export default SignalsView;
